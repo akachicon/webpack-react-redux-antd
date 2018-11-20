46d639d55b8d824d65dc5a02be988cf16109dbde
Dan Z <akachicon@gmail.com>
2018-11-20 08:17:41 +0300

    Fix middleware placement for express static

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.static('dist'));
+
 app.get('/*', (req, res) => {
   res.set('Content-Type', 'text/html');
   res.send(fs.readFileSync('./dist/index.html'));
 });
-app.use(express.static('dist'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
